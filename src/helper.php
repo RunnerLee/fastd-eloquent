37857a791d4bc45881ea7db04194e754d8d6fa51
RunnerLee <runnerleer@gmail.com>
2017-12-26 12:10:51 +0800

    update

--- src/helper.php
+++ src/helper.php
@@ -4,9 +4,21 @@
  * @email: runnerleer@gmail.com
  * @time: 2017-12
  */
+
+use Illuminate\Contracts\Support\Arrayable;
+use FastD\Http\Response;
+use FastD\Http\JsonResponse;
+
 if (!function_exists('eloquent_db')) {
     function eloquent_db($name = null)
     {
         return app()->get('eloquent')->getConnection($name);
+    }
+}
+
+if (!function_exists('eloquent')) {
+    function eloquent(Arrayable $resource, $statusCode = Response::HTTP_OK)
+    {
+        return new JsonResponse($resource->toArray(), $statusCode);
     }
 }
